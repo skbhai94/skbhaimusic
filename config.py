12f327a7f541cd3029dbecccc52152126687d730
skbhai94 <sunilkhan0158@gmail.com>
2025-03-28 23:22:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,23 +8,23 @@ from pyrogram import filters
 load_dotenv()
 
 # Telegram API credentials - Get these from the Telegram API website
-API_ID = int(getenv("API_ID")) #⚠️fill or leave
-API_HASH = getenv("API_HASH") #⚠️fill or leave
-BOT_TOKEN = getenv("BOT_TOKEN") #⚠️fill or leave 
+API_ID = int(getenv("API_ID""26605318")) #⚠️fill or leave
+API_HASH = getenv("API_HASH""8ee6af08b9b2a42868ac155250036e81") #⚠️fill or leave
+BOT_TOKEN = getenv("BOT_TOKEN""7941494972:AAHU5o5R1Tbhq8DUopZvEbPJmJv8TiY7oK4") #⚠️fill or leave 
 
 # Specify where to get the following credentials
-OWNER_USERNAME = getenv("OWNER_USERNAME", "MrBrokn") #⚠️replace 
-BOT_USERNAME = getenv("BOT_USERNAME", "MISS_YUMIPRO_BOT") #⚠️replace
-BOT_NAME = getenv("BOT_NAME", "‣ Mɪss Yᴜᴍɪ Pʀᴏ⋆ ᴠ𝟸.𝟶") #⚠️replace
-ASSUSERNAME = getenv("ASSUSERNAME", "BROKNXASSISTANT") #⚠️Replace
+OWNER_USERNAME = getenv("OWNER_USERNAME", "SKBHAI94") #⚠️replace 
+BOT_USERNAME = getenv("BOT_USERNAME", "SKMUSIX_BOT") #⚠️replace
+BOT_NAME = getenv("BOT_NAME", "‣ sᴋ ᴍᴜsɪᴄ Pʀᴏ⋆ ᴠ𝟸.𝟶") #⚠️replace
+ASSUSERNAME = getenv("ASSUSERNAME", "assistantskmusic") #⚠️Replace
 EVALOP = list(map(int, getenv("EVALOP", "1841914911").split())) 
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-LOGGER_ID = int(getenv("LOGGER_ID", -1002094142057)) #⚠️REPLACE
+LOGGER_ID = int(getenv("LOGGER_ID", -1002630443443)) #⚠️REPLACE
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 # External APIs - Get these from their respective providers
 GPT_API = getenv("GPT_API")
 DEEP_API = getenv("DEEP_API")
-OWNER_ID = int(getenv("OWNER_ID", 1841914911)) #⚠️REPLACE
+OWNER_ID = int(getenv("OWNER_ID", 7739159362)) #⚠️REPLACE
 
 # Heroku deployment settings - Refer to Heroku documentation on how to obtain these
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
@@ -34,8 +34,8 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master") #⚠️Don't Change
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Support and contact information - Provide your own support channels
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BROKENXNETWORK") #⚠️Replace
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+7yFRvkorUdFiZTll") #⚠️Replace
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/skmusicupdates") #⚠️Replace
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/skmusicupdates0") #⚠️Replace
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
